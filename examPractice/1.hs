f11cefecf0b64596601d51021594fb79b4a78396
vataid <vatai.domonkos@szgyf.gov.hu>
2022-11-17 12:30:25 +0100

    zip files added from lectures

--- examPractice/1.hs
+++ examPractice/1.hs
@@ -14,3 +14,14 @@ isEnglish x = ( x>= 'a' && x <= 'z') || (x>= 'A' && x <= 'Z')
 distance :: (Int, Int) -> Double
 distance (x, y) = sqrt ( (fromIntegral x)^^2 + (fromIntegral y)^^2 )
 
+downAndUpList :: Int -> [Int]
+downAndUpList x = toMinusXBySeven x ++ toPlusXByFive x
+
+
+toMinusXBySeven :: Int -> [Int]
+toMinusXBySeven x = [x,(x-7)..(-1*x)]
+
+toPlusXByFive :: Int -> [Int]
+toPlusXByFive x = [(-x+1),((-x+1)+5)..x]
+
+--sum [stock | (brand, cars) <- db, (type, fuel, stock) <- cars, fuel == "plug-in"]
